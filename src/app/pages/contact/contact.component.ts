c61d10dd682a896d159ab27ef3803c57c7766ba2
AdrianTeru2001 <adrianteru2001@gmail.com>
2023-05-24 12:36:17 +0200

    Modificaciones home, contact y uso-pagina / Form Contact

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,15 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit, OnDestroy {
 
-  constructor(
-    private router: Router
-  ) {}
+  private subscription: Subscription = new Subscription();
+  isLogged = false;
+  
+  constructor(public authService: AuthService){}
+
+  ngOnInit(): void {
+    this.validaciones();
+    this.subscription.add(
+      this.authService.isLogged.subscribe((resp) => (this.isLogged = resp))
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  validaciones(): void {
+    // Ejemplo de JavaScript inicial para deshabilitar el envío de formularios si hay campos no válidos
+    (function () {
+      'use strict'
+
+      // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
+      var forms = document.querySelectorAll('.needs-validation')
+
+      // Bucle sobre ellos y evitar el envío
+      Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+          form.addEventListener('submit', function (event: any) {
+            if (!form.checkValidity()) {
+              event.preventDefault()
+              event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+          }, false)
+        })
+    })()
+
+  }
 
 }
--- src/app/pages/uso-pagina/uso-pagina.component.ts
+++ src/app/pages/uso-pagina/uso-pagina.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-uso-pagina',
   templateUrl: './uso-pagina.component.html',
   styleUrls: ['./uso-pagina.component.css']
 })
-export class UsoPaginaComponent {
+export class UsoPaginaComponent implements OnInit, OnDestroy {
+
+  private subscription: Subscription = new Subscription();
+  isLogged = false;
+  
+  constructor(public authService: AuthService){}
+
+  ngOnInit(): void {
+    this.subscription.add(
+      this.authService.isLogged.subscribe((resp) => (this.isLogged = resp))
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
 }
